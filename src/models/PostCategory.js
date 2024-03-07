b9193e3cdf2bc2efd47f43c5c0bab02b51c9ec67
Lina Hsu <linahthsu@gmail.com>
2024-03-07 09:47:04 -0300

    Finaliza requisito 11 - Crie o modelo PostCategory em src/models/PostCategory.js com as propriedades e associações corretas

---
+++ src/models/PostCategory.js
@@ -0,0 +1,38 @@
+const PostCategoryModel = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+  },
+  {
+    tableName: 'categories',
+    underscored: true,
+    timestamps: false,
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = PostCategoryModel;
